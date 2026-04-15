34d91377c4587fcc90c48133a00277bb6199626e
Jordan Butler <20106583@mail.wit.ie>
2026-04-15 15:50:59 +0100

    Added userId to tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -6,7 +6,7 @@ const router = express.Router(); // eslint-disable-line
 
 // Get all tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find();
+    const tasks = await Task.find().populate('userId', 'username');
     res.status(200).json(tasks);
 });
 
@@ -15,6 +15,12 @@ router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
 }));
+
+// Get a user's tasks
+router.get('/user/:uid', async (req, res) => {
+    const tasks = await Task.find({ userId: `${req.params.uid}`});
+    res.status(200).json(tasks);
+});
 
 // Update Task
 router.put('/:id', async (req, res) => {
