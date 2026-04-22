1e671d1fc018d0abcf4d6a007de4728ad6ab33ec
Jordan Butler <20106583@mail.wit.ie>
2026-04-22 16:26:01 +0100

    Salt & Hash successfully added

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -16,20 +16,17 @@ UserSchema.statics.findByUserName = function (username) {
   return this.findOne({ username: username });
 };
 
-UserSchema.pre('save', async function(next) {
-  const saltRounds = 10; // You can adjust the number of salt rounds
-  //const user = this;
+UserSchema.pre('save', async function() {
+  const saltRounds = 12; // You can adjust the number of salt rounds
   if (this.isModified('password') || this.isNew) {
     try {
       const hash = await bcrypt.hash(this.password, saltRounds);
       this.password = hash;
-      next();
-  } catch (error) {
-     next(error);
-  }
-  } else {
-      next();
-  }
+    } catch (error) {
+      throw new Error(error);
+    }
+  } 
 });
+
 
 export default mongoose.model('User', UserSchema);
